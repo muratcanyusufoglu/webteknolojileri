1998d81eb63372c66bbaf691c3621e68666774e2
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2020-05-10 23:02:14 +0300

    altinci commit

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login</title>
     <link rel="stylesheet" href="style.css"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
